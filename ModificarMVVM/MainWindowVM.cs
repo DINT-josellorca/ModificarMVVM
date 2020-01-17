2100b19509660a26fca0b13bca048c85d72fb6b0
josellorca <jose.llorca@iesdoctorbalmis.com>
2020-01-17 10:20:06 +0100

    Agregar archivos de proyecto.

---
+++ ModificarMVVM/MainWindowVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModificarMVVM
+{
+    class MainWindowVM : INotifyPropertyChanged
+    {
+        Tema_6Entities contexto;
+        public ObservableCollection<CLIENTES> clientesCollection { get; set; }
+        public CLIENTES cliente { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public MainWindowVM()
+        {
+            contexto = new Tema_6Entities();
+            contexto.CLIENTES.Load();
+            clientesCollection = contexto.CLIENTES.Local;
+        }
+
+        public bool PuedeEjecutar()
+        {
+            return cliente != null;
+        }
+
+        public void Ejecutar()
+        {
+            contexto.SaveChanges();
+        }
+    }
+}
